58284be1f783b095c63ed0dc7c2c23171cdded0e
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-10-21 15:16:20 +0700

    ✨ F01: Login > Create LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
+import LoginPage from "./components/pages/login/LoginPage";
 
 function App() {
-  // State
-  // Je définie un useState, que j'initialise avec une chaine de caractere
-  // que je récupère dans une const, que je pourrais modifier uniquement avec son setter
-  const [prenom, setPrenom] = useState("Mister");
-
-  // Comportement
-  const handleClick = () => alert("Have a good day " + prenom);
-
-  // Affichage (render)
-  return (
-    <div>
-      <h1>Hello {prenom} </h1>
-      <button onClick={handleClick}>Hey</button>
-    </div>
-  );
+  return <LoginPage />;
 }
+
 export default App;
