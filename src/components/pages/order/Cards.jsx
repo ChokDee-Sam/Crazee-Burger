dc62e0c7187fcc48f938be14da16fb52d5cd47a0
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-11-21 21:48:53 +0700

    💄 F05 : CARDS > Improve grid-template-columns

--- src/components/pages/order/Cards.jsx
+++ src/components/pages/order/Cards.jsx
@@ -11,9 +11,11 @@ export default function Cards() {
 
 const CardsStyled = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  /* grid-template-columns: repeat(auto-fit, minmax(140px, 20%)); */
+  /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+  grid-template-columns: repeat(auto-fit, minmax(237px, 10%));
   /* grid-template-rows: auto; */
+  /* place-items: center; */
+  place-content: center;
   width: 100%;
   row-gap: 60px;
   column-gap: 90px;
